8652ee9a1112dfc8573f6359fca379bedf36051a
kdnakt <a.kid.1985@gmail.com>
2025-01-05 00:18:39 +0900

    solved iterators2

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => chars.fold(first.to_ascii_uppercase().to_string(), |acc, c| acc + &c.to_string()),
     }
 }
 
@@ -15,14 +15,14 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.into_iter().map(|&w| capitalize_first(w)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    capitalize_words_vector(words).into_iter().fold("".to_string(), |acc, w| format!("{acc}{w}"))
 }
 
 fn main() {

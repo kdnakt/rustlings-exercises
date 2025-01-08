06a2f205ab2262da7cc4515ed65b4d8a270e57f4
kdnakt <a.kid.1985@gmail.com>
2025-01-08 09:15:23 +0900

    solved iterators5

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,13 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    map.into_iter().fold(0, |acc, e| {
+        if *e.1 == value {
+            acc + 1
+        } else {
+            acc
+        }
+    })
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +55,8 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    collection.iter().map(|c| count_iterator(c, value))
+        .fold(0, |acc, v| acc + v)
 }
 
 fn main() {
